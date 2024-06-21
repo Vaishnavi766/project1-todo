b35b3d210493ec0b96da8b24ce6f918bab6f2c8f
vaishnavi <chourasiavaishnavi01@gmail.com>
2024-06-20 18:08:34 -0700

    some changes

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -19,7 +19,7 @@ function TodoForm({ addTodo }) {
                 onChange={(e) => setValue(e.target.value)}
                 placeholder="Add a new todo"
             />
-            <button type="submit">Add</button>
+            <button type="submit">Add Todo</button>
         </form>
     );
 }
