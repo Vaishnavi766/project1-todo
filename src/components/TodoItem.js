038274af31bb84102b61ceb866cc7b23f97a8bb7
vaishnavi <chourasiavaishnavi01@gmail.com>
2024-06-22 16:14:17 -0700

    something change

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ function TodoItem({ todo, index, toggleComplete, removeTodo }) {
     return (
         <li style={{ textDecoration: todo.completed ? 'line-through' : '' }}>
             {todo.text}
-            <button onClick={() => toggleComplete(index)}>Complete</button>
+            <button onclick={() => toggleComplete(index)}>Complete</button>
             <button onClick={() => removeTodo(index)}>Remove</button>
         </li>
     );
